2408a5c97e1c98dbbc98d1e2b99cd422968e0824
Andrew Maney <xxxmemescoepxxx@gmail.com>
2023-05-26 17:59:29 -0400

    Create Example.py

---
+++ Examples/Python/Example.py
@@ -0,0 +1,34 @@
+### LIBRARIES ###
+import PiShockAPI
+import time
+
+### MAIN CODE ###
+try:
+    # Initialize the API
+    api = PiShockAPI.PiShockAPI("Username", "DeviceName", "ShareCode", "APIKey")
+
+    # Test to see if the hub is online
+    if not api.IsHubOnline():
+        raise Exception("The hub is not online!")
+
+    # Send a beep and wait 2 seconds (Duration is 1 second)
+    print("Sending beep...")
+    api.SendBeep(1)
+    time.sleep(2)
+
+    # Send a vibration and wait 2 seconds (Intensity is 25, Duration is 1 second)
+    print("Sending vibration...")
+    api.SendVibration(25, 1)
+    time.sleep(2)
+
+    # Send a shock and wait 2 seconds (Intensity is 5, Duration is 1 second)
+    print("Sending shock...")
+    api.SendShock(25, 1)
+    time.sleep(2)
+
+    # Get the collar info in JSON format
+    print("Getting collar info...")
+    print("Collar info: ", api.GetCollarInfo())
+
+except Exception as ex:
+    print("[ERROR] >> ", ex)

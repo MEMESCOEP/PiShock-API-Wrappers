45c68d000ca4fe7df3e1820559bec5c0ee986230
Andrew Maney <xxxmemescoepxxx@gmail.com>
2023-05-28 23:28:39 -0400

    Update PiShockAPI.rb

--- Ruby/PiShockAPI.rb
+++ Ruby/PiShockAPI.rb
@@ -97,6 +97,31 @@ class PiShockAPI
         end
     end
 
+    # Send a shock with the specified intensity and duration of 300 milliseconds
+    def SendMiniShock (intensity = 1)
+        if intensity < 1 or intensity > 100
+            raise "Invalid intensity! The intensity must be between 1 and 100."
+        end
+
+        data = {
+            "Username": @username,
+            "Name": @computerName,
+            "Code": @shareCode,
+            "Intensity": intensity,
+            "Duration": 300,
+            "Apikey": @apiKey,
+            "Op": "0"
+        }.to_json
+
+        r = Faraday.post(@url, data, 'Content-Type'=>'application/json')
+
+        if r.status != 200
+            raise "POST failed: #{r.status}"
+        elsif r.body != "Operation Succeeded."
+            raise r.body
+        end
+    end
+
     # Retrieve information about the collar
     def GetCollarInfo()
         data = {
@@ -130,5 +155,113 @@ class PiShockAPI
         end
 
         return (jsonData['online'] != false)
+    end
+
+    # See if the collar is paused
+    def IsCollarPaused()
+        data = {
+            "Username": @username,
+            "Code": @shareCode,
+            "Apikey": @apiKey,
+        }.to_json
+
+        r = Faraday.post("https://do.pishock.com/api/GetShockerInfo", data, 'Content-Type'=>'application/json')
+        jsonData = JSON.parse(r.body)
+
+        if r.status != 200
+            raise "POST failed: #{r.status}"
+        end
+
+        return (jsonData['paused'] == false)
+    end
+
+    # Get the collar's name
+    def GetCollarName()
+        data = {
+            "Username": @username,
+            "Code": @shareCode,
+            "Apikey": @apiKey,
+        }.to_json
+
+        r = Faraday.post("https://do.pishock.com/api/GetShockerInfo", data, 'Content-Type'=>'application/json')
+        jsonData = JSON.parse(r.body)
+
+        if r.status != 200
+            raise "POST failed: #{r.status}"
+        end
+
+        return (jsonData['name'])
+    end
+
+    # Get the collar's client ID
+    def GetCollarClientID()
+        data = {
+            "Username": @username,
+            "Code": @shareCode,
+            "Apikey": @apiKey,
+        }.to_json
+
+        r = Faraday.post("https://do.pishock.com/api/GetShockerInfo", data, 'Content-Type'=>'application/json')
+        jsonData = JSON.parse(r.body)
+
+        if r.status != 200
+            raise "POST failed: #{r.status}"
+        end
+
+        return (jsonData['clientId'])
+    end
+
+    # Get the collar's ID
+    def GetCollarID()
+        data = {
+            "Username": @username,
+            "Code": @shareCode,
+            "Apikey": @apiKey,
+        }.to_json
+
+        r = Faraday.post("https://do.pishock.com/api/GetShockerInfo", data, 'Content-Type'=>'application/json')
+        jsonData = JSON.parse(r.body)
+
+        if r.status != 200
+            raise "POST failed: #{r.status}"
+        end
+
+        return (jsonData['id'])
+    end
+
+    # Get the collar's maximum intensity
+    def GetCollarMaxIntensity()
+        data = {
+            "Username": @username,
+            "Code": @shareCode,
+            "Apikey": @apiKey,
+        }.to_json
+
+        r = Faraday.post("https://do.pishock.com/api/GetShockerInfo", data, 'Content-Type'=>'application/json')
+        jsonData = JSON.parse(r.body)
+
+        if r.status != 200
+            raise "POST failed: #{r.status}"
+        end
+
+        return (jsonData['maxIntensity'])
+    end
+
+    # Get the collar's maximum duration
+    def GetCollarMaxDuration()
+        data = {
+            "Username": @username,
+            "Code": @shareCode,
+            "Apikey": @apiKey,
+        }.to_json
+
+        r = Faraday.post("https://do.pishock.com/api/GetShockerInfo", data, 'Content-Type'=>'application/json')
+        jsonData = JSON.parse(r.body)
+
+        if r.status != 200
+            raise "POST failed: #{r.status}"
+        end
+
+        return (jsonData['maxDuration'])
     end
 end
